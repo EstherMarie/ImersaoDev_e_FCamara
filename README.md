6f29298a0bdfdf62ac67a2baa246962cd09b7ec1
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2021-10-10 23:17:58 -0300

    Finalizando o projeto
    - Textos adicionados
    - README atualizado
    
    Resolve #6

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Projeto_FCamara
+# Imersao Dev & FCamara
 
 <img alt="license" src="https://img.shields.io/github/license/EstherMarie/Projeto_FCamara">
 
-> Projeto desenvolvido para o processo seletivo Imersao Dev & FCamara organizado pelo Grupo FCamara.
+> Projeto desenvolvido para o processo seletivo Imersão Dev & FCamara organizado pelo Grupo FCamara.
 
 <img alt="banner" src="./img/Banner.png">
 
@@ -12,7 +12,7 @@ O projeto é composto por uma landing page simples onde estão presentes todas a
 
 A página possui duas seções principais. A primeira traz informações a respeito do funcionamento dos escritórios e como funcionará o agendamento. A segunda seção apresenta os principais protocolos que deverão ser seguidos por todos para que o retorno ocorra com segurança.
 
-Além das seções com as informações e protocolos, também foi feito um pequeno formulário de agendamento, onde há uma verificação dos dados inseridos simulando a consulta feita ao banco de dados, confirmando se o nome e email digitados no formulário estão presentes no sistema da empresa.
+Além das seções com as informações e protocolos, também foi feito um pequeno formulário de agendamento, onde há uma verificação dos dados inseridos simulando a consulta feita ao banco de dados, confirmando se o nome e e-mail digitados no formulário estão presentes no sistema da empresa.
 
 ### Validação do formulário de agendamento
 
@@ -41,6 +41,7 @@ Para garantir que todo o conteúdo da Landing Page seja acessível a todos, o pr
 
 ## Conteúdo complementar
 
+- <a href="https://esthermarie.notion.site/Artigo-Imers-o-Dev-FCamara-2d3379b858484eafa8b25625ded20ee5">Apontamentos no Notion</a>
 - <a href="https://miro.com/app/board/o9J_ltRVNEA=/" target="_blank">Wireframe</a>
 - <a href="https://github.com/EstherMarie/Projeto_FCamara/projects/1" target="_blank">Quadro de tarefas do GitHub</a>
 
--- js/script.js
+++ js/script.js
@@ -1,91 +1,96 @@
-let usuarioAutenticado = false;
-let dadosPreenchidos = false;
-let botaoAgendamento = document.querySelector('#botao-agendamento');
-let modal = new bootstrap.Modal(document.getElementById('modal-agendamento'), {
-	keyboard: false,
-	backdrop: 'static',
+let botaoAgendamento = document.querySelector("#botao-agendamento");
+let modal = new bootstrap.Modal(document.getElementById("modal-agendamento"), {
+  keyboard: false,
+  backdrop: "static",
 });
 
 function confirmarAgendamento() {
-	// armazenando os dados inseridos em uma estrutura de dados
-	const dadosInseridos = {
-		nome: document.querySelector('#nome').value,
-		email: document.querySelector('#email').value,
-		data: document.querySelector('#data').value,
-		escritorio: document.querySelector('#escritorio-sao-paulo').checked == true ? 'São Paulo' : 'Santos',
-		estacao: document.querySelector('#estacao').value,
-	};
+  // armazenando os dados inseridos em uma estrutura de dados
+  const dadosInseridos = {
+    nome: document.querySelector("#nome").value,
+    email: document.querySelector("#email").value,
+    data: document.querySelector("#data").value,
+    escritorio:
+      document.querySelector("#escritorio-sao-paulo").checked == true
+        ? "São Paulo"
+        : "Santos",
+    estacao: document.querySelector("#estacao").value,
+  };
 
-	validacao(dadosInseridos);
+  validacao(dadosInseridos);
 }
 
 function validacao(dados) {
-	// Usuários cadastrados
-	const usuarios = [
-		{ nome: 'Jasmim', email: 'jasmim@email.com' },
-		{ nome: 'João', email: 'joao@email.com' },
-	];
+  let usuarioAutenticado = false;
+  let dadosPreenchidos = false;
 
-	// verificando se o usuário está na lista de cadastrados
-	usuarios.forEach((user) => {
-		if (dados.nome == user.nome && dados.email == user.email) {
-			usuarioAutenticado = true;
-		}
-	});
+  // Usuários cadastrados
+  const usuarios = [
+    { nome: "Jasmim", email: "jasmim@email.com" },
+    { nome: "João", email: "joao@email.com" },
+  ];
 
-	// Verificando se os campos foram preenchidos
-	for (let key in dados) {
-		if (dados[key] == '') {
-			dadosPreenchidos = false;
-			break;
-		} else {
-			dadosPreenchidos = true;
-		}
-	}
+  // verificando se o usuário está na lista de cadastrados
+  usuarios.forEach((user) => {
+    if (dados.nome == user.nome && dados.email == user.email) {
+      usuarioAutenticado = true;
+    }
+  });
 
-	if (usuarioAutenticado && dadosPreenchidos) {
-		console.log('autenticado');
-		modal.show();
-		document.querySelector('.alert').classList.add('d-none');
-		agendamentoConcluido(dados);
-	} else {
-		console.log('invalido');
-		document.querySelector('.alert').classList.remove('d-none');
+  // Verificando se os campos foram preenchidos
+  for (let key in dados) {
+    if (dados[key] == "") {
+      dadosPreenchidos = false;
+      break;
+    } else {
+      dadosPreenchidos = true;
+    }
+  }
 
-		modal.hide();
-	}
+  if (usuarioAutenticado && dadosPreenchidos) {
+    modal.show();
+    document.querySelector(".alert").classList.add("d-none");
+    agendamentoConcluido(dados);
+  } else {
+    document.querySelector(".alert").classList.remove("d-none");
+    modal.hide();
+  }
 }
 
 function agendamentoConcluido(dados) {
-	let h4 = document.createElement('h4');
-	h4.textContent = `Olá, ${dados.nome}!`;
+  let h4 = document.createElement("h4");
+  h4.textContent = `Olá, ${dados.nome}!`;
 
-	let p = document.createElement('p');
-	p.textContent = `Você está agendado(a) para o dia ${document.querySelector('#data').valueAsDate.getDate() + 1} / ${
-		document.querySelector('#data').valueAsDate.getMonth() + 1
-	} , no escritório em ${dados.escritorio} e a sua estação de trabalho é a número ${
-		dados.estacao
-	}. O comprovante de agendamento foi enviado para o seu email.`;
+  let p = document.createElement("p");
+  p.textContent = `Você está agendado(a) para o dia ${
+    document.querySelector("#data").valueAsDate.getDate() + 1
+  } / ${
+    document.querySelector("#data").valueAsDate.getMonth() + 1
+  } , no escritório em ${
+    dados.escritorio
+  } e a sua estação de trabalho é a número ${
+    dados.estacao
+  }. O comprovante de agendamento foi enviado para o seu email.`;
 
-	let modalContent = document.querySelector('.modal-body');
-	modalContent.appendChild(h4);
-	modalContent.append(p);
+  let modalContent = document.querySelector(".modal-body");
+  modalContent.appendChild(h4);
+  modalContent.append(p);
 
-	// desabilitando o botão submit
-	document.querySelector('#botao-agendamento').disabled = true;
+  // desabilitando o botão submit
+  botaoAgendamento.disabled = true;
 
-	// desabilitando os campos input
-	document.querySelector('select').value = '';
-	let listaInput = document.querySelectorAll('input');
+  // desabilitando os campos input
+  document.querySelector("select").value = "";
+  let listaInput = document.querySelectorAll("input");
 
-	for (let i = 0; i < listaInput.length; i++) {
-		listaInput[i].value = '';
-		listaInput[i].disabled = true;
-		listaInput[i].checked = false;
-	}
+  for (let i = 0; i < listaInput.length; i++) {
+    listaInput[i].value = "";
+    listaInput[i].disabled = true;
+    listaInput[i].checked = false;
+  }
 }
 
-botaoAgendamento.addEventListener('click', (event) => {
-	confirmarAgendamento();
-	event.preventDefault();
+botaoAgendamento.addEventListener("click", (event) => {
+  confirmarAgendamento();
+  event.preventDefault();
 });
